72ccf040022b81f63538c1074440c3304388a89a
mahad-basit <mahadbasit.12345@gmail.com>
2026-06-06 19:02:26 +0500

    Add grade-checker and odd-even-checker

---
+++ grade-checker/grade.py
@@ -0,0 +1,19 @@
+marks = int(input("Enter your marks: "))
+
+if marks > 100 or marks < 0:
+    print("Invalid Input!")
+
+elif marks >= 90:
+    print("Grade: A")
+
+elif marks >= 80:
+    print("Grade: B")
+
+elif marks >= 70:
+    print("Grade: C")
+
+elif marks >= 60:
+    print("Grade: D")
+
+else: 
+    print("Grade: F") 
---
+++ odd-even-checker/odd.py
@@ -0,0 +1,7 @@
+num = int(input("Input any number: "))
+
+if num % 2 == 0:
+    print("Even")
+
+else:
+    print("Odd")
