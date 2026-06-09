c136ec3d78c3f829798418c2b440e5ebf1de33b2
mahad-basit <mahadbasit.12345@gmail.com>
2026-06-09 09:20:09 +0500

    Add number guessing game with guess counter

---
+++ number-guessing-game/guess.py
@@ -0,0 +1,19 @@
+import random 
+
+number = random.randint(1,100)
+counter = 0
+
+while True:
+    
+    guess = int(input("Guess the number: "))
+    counter += 1
+    if guess > number:
+        print("Too High")
+
+    elif guess < number:
+        print("Too Low")
+
+    else:
+        print("You got it")
+        print(f"Tries: {counter}")
+        break
